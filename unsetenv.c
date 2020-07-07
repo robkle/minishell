520df18bd892b4cddceb8fc4f920213047fe60da
Robert Klein <rklein@c3r2p7.hive.fi>
2020-07-07 13:56:19 +0300

    free unallocated memory bug in unsetenv fixed

--- unsetenv.c
+++ unsetenv.c
@@ -6,23 +6,20 @@
 /*   By: rklein <rklein@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/17 16:15:31 by rklein            #+#    #+#             */
-/*   Updated: 2020/06/30 14:44:04 by rklein           ###   ########.fr       */
+/*   Updated: 2020/07/07 13:53:52 by rklein           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	ft_unsetenv(t_sh *sh)
+static void	ft_unsetenv_update(t_sh *sh, int size)
 {
 	char	**tmp;
+	int		len;
 	int		i;
 	int		j;
-	int		len;
 
-	i = 0;
-	while (sh->env[i])
-		i++;
-	tmp = (char**)malloc(sizeof(char*) * i);
+	tmp = (char**)malloc(sizeof(char*) * size);
 	len = ft_strlen(sh->par[1]);
 	i = 0;
 	j = 0;
@@ -37,4 +34,23 @@ void	ft_unsetenv(t_sh *sh)
 	tmp[j] = NULL;
 	ft_free_array(sh->env);
 	sh->env = tmp;
+}
+
+void		ft_unsetenv(t_sh *sh)
+{
+	int		i;
+	int		len;
+	_Bool	listed;
+
+	i = -1;
+	listed = 0;
+	len = ft_strlen(sh->par[1]);
+	while (sh->env[++i])
+	{
+		if (ft_strncmp(sh->env[i], sh->par[1], len) == 0 &&
+				sh->env[i][len] == '=')
+			listed = 1;
+	}
+	if (listed)
+		ft_unsetenv_update(sh, i);
 }
